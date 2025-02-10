4e5a005ae2be1191b1563ba40d8a1573322c0788
Tojo rasii <tojorasii1619@gmail.com>
2025-02-10 10:27:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let index = 0;
+const track = document.getElementById("carousel-img-article");
+const totalImages = document.querySelectorAll(".carousel-img-article img").length;
+
+function scrollCarousel() {
+    index = (index + 1) % totalImages;
+    track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(scrollCarousel, 5000);
